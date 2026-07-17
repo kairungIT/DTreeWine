94db17e91369eed72b5d267f19977c4885e3704c
kairungIT <113651426+kairungIT@users.noreply.github.com>
2026-07-17 15:25:38 +0700

    .

--- home.py
+++ home.py
@@ -1,26 +1,26 @@
-import json
-import time
-import requests
-
 import streamlit as st
 from streamlit_lottie import st_lottie
-from streamlit_lottie import st_lottie_spinner
+import requests
+import json
 
+# โหลด Lottie จาก URL
 def load_lottieurl(url: str):
     r = requests.get(url)
     if r.status_code != 200:
         return None
     return r.json()
 
-lottie_url_hello = "https://lottie.host/3f647b41-61bf-4d39-93c3-0433420604cc/8NtmEbAWmO.json"
-lottie_url_download = "https://lottie.host/290016a6-d650-4a32-85c7-4d77b0a892ca/4rNL8XZeZt.json"
-lottie_hello = load_lottieurl(lottie_url_hello)
-lottie_download = load_lottieurl(lottie_url_download)
+# ตัวอย่างการใช้งาน
+lottie_hello = load_lottieurl("https://assets2.lottiefiles.com/packages/lf20_Hs2j1y.json")
 
-
-st_lottie(lottie_hello, key="hello")
-
-if st.button("Download"):
-    with st_lottie_spinner(lottie_download, key="download"):
-        time.sleep(5)
-    st.balloons()
+if lottie_hello:
+    st_lottie(
+        lottie_hello,
+        speed=1,
+        reverse=False,
+        loop=True,
+        quality="high",  # low | medium | high
+        height=300,
+        width=300,
+        key="hello"
+    )
